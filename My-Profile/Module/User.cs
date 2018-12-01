7ba74d82d34de5d80241d3b122f5fdf54f89a8b4
narendar kumar <nkumar010896@gmail.com>
2018-12-01 10:49:53 +0530

    docker compose added

--- My-Profile/Module/User.cs
+++ My-Profile/Module/User.cs
@@ -6,6 +6,7 @@ namespace My_Profile
     public class User
     {
         [BsonId]
+        
         public MongoDB.Bson.ObjectId UserId { get; set; }
         public string UserName { get; set; }
         public string FullName { get; set; }
