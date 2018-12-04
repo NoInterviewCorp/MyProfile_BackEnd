e8037f0ac65b7ed33df7adb31672431b9a93d21c
Soham Sahare <sohamsahare@gmail.com>
2018-12-04 05:38:22 +0000

    fixed hostname in rabbitmqconnection

--- My-Profile/entrypoint.sh
+++ My-Profile/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 run_cmd="dotnet run"
-./wait-for-it.sh -t 0 localhost:27019 -- echo "mongodb is up"
+./wait-for-it.sh -t 0 mongop:27019 -- echo "mongodb is up"
 >&2 echo "MongoDB is Up"
 exec $run_cmd
